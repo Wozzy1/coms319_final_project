59db64ea30b6dd954fb345facca76d9e4e2090c5
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2024-12-06 17:57:08 -0600

    created basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './NavBar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <div className="d-flex">
+            <NavigationBar />
+        </div>
+        <Routes>
+          <Route path="/" element={<div>This is the homepage, later add a Homepage.js</div>}></Route>
+          <Route path="/view_comments" element={<div>This will be a list of comments</div>}></Route>
+        </Routes>
+    </Router>
+</div>
+);
 }
 
 export default App;
