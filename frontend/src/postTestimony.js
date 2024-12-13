289b448242503a573e2ab3ac542818ea65005ef1
Zhi <zhiyangteh@gmail.com>
2024-12-13 16:44:18 -0600

    Revert "fetch username of testimony poster"
    This reverts commit 0e76d78bbf41de5564e37912ef12ad8bccefdf11.
    
    revert 1

---
+++ frontend/src/postTestimony.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function postTestimony() {
+  return (
+    <div className='container-fluid'>
+      <header className='App-header'>Log In</header>
+      <body>yes</body>
+    </div>
+  );
+}
+
+export default postTestimony;
